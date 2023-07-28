f25e1faeeb2ff7149c37c9190a1987c39b09b743
Ilya Vassyutovich <vassyutovich@mindbox.cloud>
2023-07-28 13:38:34 +0300

    Refactor fixup-filter to use shared lib for commit message check

--- pre-push/fixup-filter/xx-fixup-filter.hook.ps1
+++ pre-push/fixup-filter/xx-fixup-filter.hook.ps1
@@ -25,11 +25,14 @@ param (
 	$RemoteSha
 )
 
-$DebugPreference = "Continue"
+$DebugPreference = "SilentlyContinue"
 $ErrorActionPreference = "Stop"
 
-Write-Host "<< fixup-filter, ps-0.1"
+Write-Host "<< fixup-filter, ps-1.1"
 Write-Debug "Begin fixup-filter hook"
+
+. (Join-Path $PSScriptRoot "./lib/Check-CommitMessage.ps1")
+
 
 $SuppliedParams = @{
 	RemoteName = $RemoteName;
@@ -44,41 +47,18 @@ $SuppliedParams = @{
 Write-Debug "Supplied params"
 Write-Debug ($SuppliedParams | Out-String)
 
-$Z40 = "0000000000000000000000000000000000000000"
-$HookExitCode = 0
 
-if ($LocalSha -eq $Z40) {
-	Write-Debug "Local sha is null, handling `"delete`", NOOP"
+$HookExitCode, $ForbiddenCommitSHA = Check-CommitMessage `
+	-RemoteRef $RemoteRef `
+	-RemoteSha $RemoteSha `
+	-LocalSha $LocalSha `
+	-MessageRegex "^fixup!"
+if ($null -ne $ForbiddenCommitSHA) {
+	Write-Host `
+		-ForegroundColor Red `
+		"Forbidden commit detected — $ForbiddenCommitSHA"
 }
-else {
-	Write-Debug "Local sha is not null"
 
-	if ($RemoteRef -eq "refs/heads/master" -or $RemoteRef -eq "refs/heads/main") {
-		Write-Debug "Handling push to $RemoteRef"
-		if ($RemoteSha -eq $Z40) {
-			Write-Debug "Remote sha is null"
-			$Range = $LocalSha
-		}
-		else {
-			Write-Debug "Remote sha is not null — setting range"
-			$Range = "$RemoteSha..$LocalSha"
-		}
-		Write-Debug "Range is `"$Range`""
-	
-		$ForbiddenCommit = (git rev-list --max-count=1 --grep "^fixup!" "$Range")
-		if ($null -ne $ForbiddenCommit) {
-			Write-Debug "Forbidden commit detected"
-			Write-Warning "<< Push rejected for commit `"$ForbiddenCommit`"."
-			$HookExitCode = 13
-		}
-		else {
-			Write-Debug "No forbidden commits detected"
-		}
-	}
-	else {
-		Write-Debug "Remote ref/branch is `"unprotected`", NOOP"
-	}
-}
 
 Write-Debug "End fixup-filter hook"
 exit $HookExitCode
